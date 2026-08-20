0f8de7aac9c9f88ad49e51835dd36d2d19eb662e
AndrewGRM <andrew.grm15@gmail.com>
2026-08-19 22:14:53 -0300

    Criando atributos do Usuário

--- src/main/java/com/Fibra/usuario/model/Usuario.java
+++ src/main/java/com/Fibra/usuario/model/Usuario.java
@@ -1,4 +1,36 @@
 package com.Fibra.usuario.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+
+    // Utilizar um numero de matricula auto gerado pelo banco ou CPF
+    Integer matricula;
+
+    String username;
+
+    String email;
+    //Trocar para tipo para password em breve
+    String password;
+
+    int idade;
+
+    Double peso;
+
+    Double altura;
+
+    String genero;
+
+    Boolean role; // ADMIN, USER
+
+    List<String> unidadeMedida;
+
+    private Boolean enabled;
 }
